15497b1b5c3127aaf465d419784e73fd1db47b54
thisishwan2 <feel000617@gmail.com>
2023-12-18 00:27:31 +0900

    feat: spring 생성

---
+++ src/main/java/com/donggukthon/Showman/ShowmanApplication.java
@@ -0,0 +1,13 @@
+package com.donggukthon.Showman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowmanApplication.class, args);
+	}
+
+}
