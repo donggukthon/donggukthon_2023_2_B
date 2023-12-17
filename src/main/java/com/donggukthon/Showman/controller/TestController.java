22b1cd3a7c0600d15835d4b73f1038cd2f7fe395
thisishwan2 <feel000617@gmail.com>
2023-12-18 03:40:35 +0900

    feat: health-check controller 추가

---
+++ src/main/java/com/donggukthon/Showman/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.donggukthon.Showman.controller;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
