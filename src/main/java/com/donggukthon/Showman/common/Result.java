b44fa47c9a3ab1d1bb155c0eba5a37ee53695409
thisishwan2 <feel000617@gmail.com>
2023-12-18 23:37:18 +0900

    feat: 응답코드 추가

--- src/main/java/com/donggukthon/Showman/common/Result.java
+++ src/main/java/com/donggukthon/Showman/common/Result.java
@@ -17,8 +17,15 @@ public enum Result {
     FORBIDDEN_REQUEST(403, "접근이 금지되었습니다."),
     EXPIRED_TOKEN(403, "만료된 토큰입니다."),
     INVALID_TOKEN_SIGNATURE(403, "잘못된 토큰 서명입니다."),
-    INVALID_TOKEN_FORMAT(403, "잘못된 토큰 형식입니다."),;
+    INVALID_TOKEN_FORMAT(403, "잘못된 토큰 형식입니다."),
 
+    // user
+    NOT_FOUND_USER(404, "존재하지 않는 사용자입니다."),
+
+    // posting
+    NOT_FOUND_POSTING(404, "존재하지 않는 게시글입니다."),
+
+    ;
 
     private final String message;
     private final int status;
