0c316aec536577a79aa1f329ea9cc27fa1b2114e
thisishwan2 <feel000617@gmail.com>
2023-12-19 01:26:39 +0900

    fix: run을 위한 주석 처리

--- src/main/java/com/donggukthon/Showman/controller/UserController.java
+++ src/main/java/com/donggukthon/Showman/controller/UserController.java
@@ -9,11 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
-import com.donggukthon.Showman.service.UserService;
-import com.donggukthon.Showman.dto.UserProfileResponse;
-import com.donggukthon.Showman.dto.UpdateUserRequest;
-import com.donggukthon.Showman.global.auth.token.jwt.JwtAuthenticationRecord;
-import com.donggukthon.Showman.global.base.ApiResponse;
+import com.donggukthon.Showman.service.user.UserService;
+import com.donggukthon.Showman.dto.user.response.UserProfileResponse;
+import com.donggukthon.Showman.dto.user.request.UpdateUserRequest;
 
 import jakarta.validation.Valid;
 
@@ -28,32 +26,32 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping(value = "/me", produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<ApiResponse<UserProfileResponse>> getMyProfile(
-            @AuthenticationPrincipal JwtAuthenticationRecord user
-    ) {
-        UserProfileResponse response = userService.getUserProfile(user.id());
-
-        return ResponseEntity.ok().body(new ApiResponse<>(response));
-    }
-
-    @PutMapping(value = "/me", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<ApiResponse<UserProfileResponse>> updateMyProfile(
-            @RequestBody @Valid UpdateUserRequest updateUserRequest,
-            @AuthenticationPrincipal JwtAuthenticationRecord user
-    ) {
-        UserProfileResponse response = userService.updateUserProfile(updateUserRequest, user.id());
-
-        return ResponseEntity.ok().body(new ApiResponse<>(response));
-    }
-
-    @GetMapping(value = "/{userId}", produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<ApiResponse<UserProfileResponse>> getUserProfile(
-            @PathVariable Long userId
-    ) {
-        UserProfileResponse response = userService.getUserProfile(userId);
-
-        return ResponseEntity.ok().body(new ApiResponse<>(response));
-    }
+//    @GetMapping(value = "/me", produces = APPLICATION_JSON_VALUE)
+//    public ResponseEntity<ApiResponse<UserProfileResponse>> getMyProfile(
+//            @AuthenticationPrincipal JwtAuthenticationRecord user
+//    ) {
+//        UserProfileResponse response = userService.getUserProfile(user.id());
+//
+//        return ResponseEntity.ok().body(new ApiResponse<>(response));
+//    }
+//
+//    @PutMapping(value = "/me", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+//    public ResponseEntity<ApiResponse<UserProfileResponse>> updateMyProfile(
+//            @RequestBody @Valid UpdateUserRequest updateUserRequest,
+//            @AuthenticationPrincipal JwtAuthenticationRecord user
+//    ) {
+//        UserProfileResponse response = userService.updateUserProfile(updateUserRequest, user.id());
+//
+//        return ResponseEntity.ok().body(new ApiResponse<>(response));
+//    }
+//
+//    @GetMapping(value = "/{userId}", produces = APPLICATION_JSON_VALUE)
+//    public ResponseEntity<ApiResponse<UserProfileResponse>> getUserProfile(
+//            @PathVariable Long userId
+//    ) {
+//        UserProfileResponse response = userService.getUserProfile(userId);
+//
+//        return ResponseEntity.ok().body(new ApiResponse<>(response));
+//    }
 }
 
