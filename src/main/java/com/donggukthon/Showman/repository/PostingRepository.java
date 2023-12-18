45444997a2cf542f8919fa8c80da73ffa1fa9649
thisishwan2 <feel000617@gmail.com>
2023-12-18 23:37:43 +0900

    feat: posting 레포지토리 추가

---
+++ src/main/java/com/donggukthon/Showman/repository/PostingRepository.java
@@ -0,0 +1,7 @@
+package com.donggukthon.Showman.repository;
+
+import com.donggukthon.Showman.entity.Posting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostingRepository extends JpaRepository<Posting, Long> {
+}
