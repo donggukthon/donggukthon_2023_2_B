141923a2c5e1d0f359c62bb044d606473ef7ca4c
thisishwan2 <feel000617@gmail.com>
2023-12-19 21:26:40 +0900

    feat: 위치 기반 조회 네이티브 쿼리 추가

--- src/main/java/com/donggukthon/Showman/repository/PostingRepository.java
+++ src/main/java/com/donggukthon/Showman/repository/PostingRepository.java
@@ -3,12 +3,32 @@ package com.donggukthon.Showman.repository;
 import com.donggukthon.Showman.entity.Posting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostingRepository extends JpaRepository<Posting, Long> {
 
-    @Query("SELECT p FROM Posting p WHERE p.latitude BETWEEN ?1 AND ?2 AND p.longitude BETWEEN ?3 AND ?4 ORDER BY FUNCTION('distance', p.latitude, p.longitude, ?5, ?6)")
-    List<Posting> findInBoundingBoxOrderedByDistance(Double minLat, Double maxLat, Double minLon, Double maxLon, Double originLat, Double originLon);
+    @Query(value = "select * from posting\n" +
+            "where latitude between :startX and :endX and longitude between :startY and :endY ;",
+            nativeQuery = true)
+    List<Posting> findWithinMap(@Param("startX") Double startX,
+                                @Param("endX") Double endX,
+                                @Param("startY") Double startY,
+                                @Param("endY") Double endY);
+
+    @Query(value = "select *, (6371*acos(cos(radians(latitude))*cos(radians(:curMemberX))*cos(radians(:curMemberY)-radians(longitude))\n" +
+            "+sin(radians(latitude))*sin(radians(:curMemberX)))) AS distance\n" +
+            "from posting\n" +
+            "where latitude between :startX and :endX and longitude between :startY and :endY " +
+            "having distance <= 10\n" +
+            "order by distance DESC;",
+            nativeQuery = true)
+    List<Posting> findWithinRadius(@Param("curMemberX") Double curMemberX,
+                                   @Param("curMemberY") Double curMemberY,
+                                   @Param("startX") Double startX,
+                                   @Param("endX") Double endX,
+                                   @Param("startY") Double startY,
+                                   @Param("endY") Double endY);
 
 }
