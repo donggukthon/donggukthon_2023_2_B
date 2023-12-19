24536f39cb390c323bce149045ed1f7c914392e5
thisishwan2 <feel000617@gmail.com>
2023-12-20 04:36:21 +0900

    fix: cors 수정

--- src/main/java/com/donggukthon/Showman/config/CorsConfig.java
+++ src/main/java/com/donggukthon/Showman/config/CorsConfig.java
@@ -16,9 +16,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        //config.addAllowedOrigin("http://localhost:3000");
-        //config.addAllowedOrigin("http://localhost:80")
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
