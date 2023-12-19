5aa633627e839d55748a43507e50e4e7da0c1e4e
thisishwan2 <feel000617@gmail.com>
2023-12-19 17:40:04 +0900

    feat: 위치 데이터를 위한 엔티티 및 유틸

--- src/main/java/com/donggukthon/Showman/entity/Posting.java
+++ src/main/java/com/donggukthon/Showman/entity/Posting.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Point;
 
 @Entity
 @Getter
@@ -28,6 +29,8 @@ public class Posting extends BaseEntity{
     private String address;
 
     private String snowmanIdCardImageUrl;
+
+    private Point point;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
